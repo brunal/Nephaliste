bd38f8af2743459aff1c368bee49852801ac92b9
Bruno Cauet <brunocauet@gmail.com>
2011-07-20 16:01:15 +0200

    Modèles complétés

--- Nephaliste/bar/models.py
+++ Nephaliste/bar/models.py
@@ -1,5 +1,56 @@
 #coding=utf-8
 from django.db import models
+from datetime import date
+
+class Consommation(models.Model):
+	"""
+	Chaque objet est une chose que la Coopé vend
+
+	>>> conso = Consommation.objects.create(nom="Pinte de Kro", prix="2.00")
+	>>> conso
+	'Pinte de Kro à 2.00€'
+
+	"""
+
+	nom = models.CharField(max_length=50)
+	disponible = models.BooleanField(default=True)
+	prix = models.DecimalField(max_digits=4, decimal_places=2)
+
+	def __unicode__(self):
+		nom = self.nom + u" à " + self.prix + "€"
+		if(not disponible):
+			nom += " (indisponible)"
+		return nom
+
+class Depot(models.Model):
+	"""
+	Classe retenant l'argent déposé par un client
+	"""
+	TYPES = (
+			(0, u'cash'),
+			(1, u'chèque'),
+			)
+	type = models.IntegerField(choices=TYPES)
+	date = models.DateTimeField(auto_now_add=True)
+	montant = models.DecimalField(max_digits=5, decimal_places=2)
+	user = models.ForeignKey('User')
+
+	def __unicode__(self):
+		return u"Dépôt par " + self.user + " de " + self.montant + "€  le " + self.date + " (" + self.get_type_display() + ")"
+
+
+class Historique(models.Model):
+	"""
+	Classe retenant toutes les commandes passées
+	"""
+
+	user = models.ForeignKey('User')
+	consommation = models.ForeignKey('Consommation')
+	date = models.DateTimeField(auto_now_add=True)
+
+	def __unicode__(self):
+		return self.consommation + " par " + self.user + " le " + self.date
+
 
 class Promotion(models.Model):
 	"""
@@ -14,6 +65,7 @@ class Promotion(models.Model):
 		"""
 		Retourne vrai quand les membres de cette promotion ont peu de chance d'être encore à l'école
 
+		>>> from datetime import datetime
 		>>> datetime.today().year
 		2011
 		>>> promo = Promotion.objects.create(annee=2009)
@@ -74,4 +126,12 @@ class User(models.Model):
 		Débite une boisson à un consommateur
 		"""
 		commande = Historique.objects.create(user=self, consommation=consommation)
+		return commande
+
+	def ajouterCaution(self):
+		"""
+		Ajoute une caution à un utilisateur
+		"""
+		self.caution = date.today()
+		return
 
