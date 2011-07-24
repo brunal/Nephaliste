f8426d4a66ddfe180fad4671b1e394c13dbf03ec
Bruno Cauet <brunocauet@gmail.com>
2011-07-24 20:33:43 +0200

    Added views

--- Nephaliste/bar/views.py
+++ Nephaliste/bar/views.py
@@ -2,11 +2,35 @@
 from django.shortcuts import render_to_response
 from models import *
 
-def comptoir(request):
+def comptoir(request, error="", message=""):
 	"""
 	Cette page liste les boissons disponibles et propose de consommer
 	"""
 	consommations = Consommation.objects.filter(disponible=True)
 	consommations = sorted(consommations, key=lambda conso: -conso.popularite())
 	
-	return render_to_response('bar/comptoir.html', {'consommations': consommations})
+	return render_to_response('bar/comptoir.html', {'consommations': consommations, 'error': error, 'message': message})
+
+def commander(request):
+	"""
+	Cette page effectue la commande d'une personne (données POST)
+	"""
+	if not request.method == "POST":
+		return comptoir(request, error="Page accessible uniquement en POST")
+
+	#On traite les données reçues
+
+	message=""
+	return comptoir(request, message=message)
+
+def crediter(request):
+	"""
+	Cette page crédite le compte ciblé (données POST)
+	"""
+	if not request.method == "POST":
+		return comptoir(request, error="Page accessible uniquement en POST")
+
+	#On traite les données reçues
+
+	message=""
+	return comptoir(request, message=message)
